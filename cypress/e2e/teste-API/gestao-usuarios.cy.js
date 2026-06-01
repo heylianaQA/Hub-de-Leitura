4b6b765e5036f323931905993af2889230fb3d4b
heyannli <heyliana.qa@gmail.com>
2026-06-01 19:27:20 -0300

    .yaml update

--- cypress/e2e/teste-API/gestao-usuarios.cy.js
+++ cypress/e2e/teste-API/gestao-usuarios.cy.js
@@ -140,7 +140,7 @@ describe("DELETE : Teste de API - Gestão de Usuários", () => {
   it("Deve deletar usuário com sucesso", () => {
     cy.api({
       method: "DELETE",
-      url: "api/users/67",
+      url: "api/users/68",
       headers: { Authorization: token },
     }).should((response) => {
       (expect(response.status).to.equal(200),
