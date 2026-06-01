0657362163a423087a577fb4eb8e4543f262e0a5
heyannli <heyliana.qa@gmail.com>
2026-06-01 19:17:38 -0300

    atualização do Cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
   allowCypressEnv: true,
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
       return config;
     },
     baseUrl: "http://localhost:3000/",
